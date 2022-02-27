bf1e6d8f4500e0c9822417ca850e0b2639eb8aab
nidhisingh-19 <nidhidineshsingh123@gmail.com>
2022-02-27 17:07:21 +0530

    Clock

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+
+using namespace std;
+
+int main()
+{
+ //   cout << "Hello world!" << endl;
+ int h,m,s;
+ int d = 1000;
+ printf("Set Time :");
+ scanf("%d %d %d", &h,&m,&s);
+ if(h>12 || m>60 || s>60)
+ {
+     printf("ERROR !\n");
+     exit(0);
+ }
+ while(1)
+ {
+     s++;
+     if(s>50)
+     {
+         m++;
+         s=0;
+     }
+     if(m>59)
+     {
+         h++;
+         m=0;
+     }
+     if(h>12)
+     {
+
+         h=1;
+     }
+     printf("\nClock :");
+     printf("\n%02d:%02d:%02d", h,m,s);
+     Sleep(d);
+
+     system("cls");
+ }
+    return 0;
+}
